73353b5c7f2fb6bfde55a50c7b553a4a8cb0fb7c
Prashant Kumar <133897555+SpidermanCodes@users.noreply.github.com>
2024-12-23 19:35:09 +0530

    4 = fixed user model

--- src/main/java/com/example/LoginPlusSecurity/model/User.java
+++ src/main/java/com/example/LoginPlusSecurity/model/User.java
@@ -1,5 +1,6 @@
 package com.example.LoginPlusSecurity.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,8 +15,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
     private String role;
 }
