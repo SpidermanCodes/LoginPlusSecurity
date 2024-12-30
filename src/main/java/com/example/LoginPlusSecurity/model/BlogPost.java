aee3a843e9cfe010f6571d436d1bd9834c6f4256
Prashant Kumar <133897555+SpidermanCodes@users.noreply.github.com>
2024-12-30 21:20:39 +0530

    13 = new branch + TinyMCE

--- src/main/java/com/example/LoginPlusSecurity/model/BlogPost.java
+++ src/main/java/com/example/LoginPlusSecurity/model/BlogPost.java
@@ -1,9 +1,12 @@
 package com.example.LoginPlusSecurity.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -25,7 +29,7 @@ public class BlogPost {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false, length = 10000)
+    @Column(columnDefinition = "MEDIUMTEXT", nullable = false)
     private String content;
 
     @Column(nullable = false)
