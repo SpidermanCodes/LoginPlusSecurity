2ff3b8d1d729ecd22890aed4e7a710a550072129
Prashant Kumar <133897555+SpidermanCodes@users.noreply.github.com>
2024-12-31 07:58:02 +0530

    13 = UI-interation very first checkpoint

--- src/main/java/com/example/LoginPlusSecurity/model/BlogPost.java
+++ src/main/java/com/example/LoginPlusSecurity/model/BlogPost.java
@@ -1,12 +1,9 @@
 package com.example.LoginPlusSecurity.model;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -15,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
